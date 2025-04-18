6ce05d52be2b1b745f2aefc421569d8cac66e3a1
okashadev <ukashahashim231@gmail.com>
2025-04-18 09:14:31 +0500

    sign in screen done

---
+++ src/app/sign-in/page.jsx
@@ -0,0 +1,98 @@
+"use client";
+import Image from "next/image";
+
+const page = () => {
+  return (
+    <>
+      <div className="h-screen w-full bg-gray-200 flex justify-center items-center">
+        <div className="bg-white flex flex-col justify-center p-4 rounded-lg shadow-lg">
+          <div className="flex flex-col items-center justify-center">
+            <Image
+              src="/images/logo/NewWebLogo.png"
+              alt="logo"
+              width={250}
+              height={50}
+            />
+            <p className="text-gray-700 text-sm">
+              Join the developer community
+            </p>
+          </div>
+          <h1 className="text-xl text-[#1d4ed8] text-center font-semibold mt-3">
+            Sign In
+          </h1>
+          <div className="border-b border-b-gray-300 shadow w-full h-2 px-4 my-3"></div>
+          <form className="w-full flex flex-col">
+            <div className="flex flex-col mb-4 mt-3">
+              <label
+                htmlFor="email"
+                className="text-gray-700 ms-1 text-sm font-normal "
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                className="border w-96 shadow-lg border-gray-300 rounded-lg px-3 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-[#1d4ed8]"
+              />
+            </div>
+            <div className="flex flex-col mb-4 mt-2">
+              <label
+                htmlFor="email"
+                className="text-gray-700 ms-1 text-sm font-normal "
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                className="border w-96 shadow-lg border-gray-300 rounded-lg px-3 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-[#1d4ed8]"
+              />
+            </div>
+            <div className="flex justify-between items-center px-2 my-1">
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="remember"
+                  className="w-4 h-4 border-gray-300 rounded focus:ring-[#1d4ed8]"
+                />
+                <label
+                  htmlFor="remember"
+                  className="text-gray-700 ms-2 text-sm font-normal "
+                >
+                  Remember me
+                </label>
+              </div>
+              <a
+                href="#"
+                className="text-[#1d4ed8] hover:underline text-sm font-normal"
+              >
+                Forgot password?
+              </a>
+            </div>
+            <div className="px-5 my-4">
+              <button type="submit" className="bg-[#1d4ed8] cursor-pointer text-white rounded-lg w-full pt-1 pb-2 mt-3">
+                Sign in to your account
+              </button>
+            </div>
+          </form>
+          <div className="border-b border-b-gray-300 shadow w-full h-2 px-4 my-3"></div>
+          <div>
+            <p className="text-gray-700 text-sm text-center">
+              Don't have an account?{" "}
+              <a
+                href="/sign-up"
+                className="text-[#1d4ed8] hover:underline font-semibold"
+              >
+                Sign up
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
